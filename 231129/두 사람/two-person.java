daa66c09a1ed81374d9d13db15f0e881822661bb
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-11-29 11:21:55 +0900

    Title: 두 사람, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231129/두 사람/two-person.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int firstAge = sc.nextInt();
+        char firstSex = sc.next().charAt(0);
+        int secondAge = sc.nextInt();
+        char secondSex = sc.next().charAt(0);
+
+        if (firstSex == 'M' || secondSex == 'M') {
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
