1d332964b452c48d59040474abc6e04d6c60c8a5
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-07-24 20:06:05 +0900

    Title: 뿌요뿌요, Time: 274ms, Memory: 19MB, Status: Passed - CodeTree

--- 240724/뿌요뿌요/puyo-puyo.java
+++ 240724/뿌요뿌요/puyo-puyo.java
@@ -46,7 +46,7 @@ public class Main {
         cnt++;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (Math.abs(i) == Math.abs(j)) continue;
+                if (Math.abs(delta[i]) == Math.abs(delta[j])) continue;
                 int nx = sx + delta[i];
                 int ny = sy + delta[j];
 
