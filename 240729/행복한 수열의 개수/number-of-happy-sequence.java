fa6e4312d0a8b2ea6470c2f25701963ebb46d3e5
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-07-29 17:51:53 +0900

    Title: 행복한 수열의 개수, Time: 313ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 240729/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int ans;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            int rowNum = matrix[0][i];
+            int colNum = matrix[i][0];
+            int rowCnt = 1;
+            int colCnt = 1;
+
+            for (int j = 1; j < n; j++) {
+
+                if (matrix[i][j] == colNum) {
+                    colCnt++;
+                } else {
+                    colCnt = 1;
+                    colNum = matrix[i][j];
+                }
+                
+                if (matrix[j][i] == rowNum) {
+                    rowCnt++;
+                } else {
+                    rowCnt = 1;
+                    rowNum = matrix[j][i];
+
+                }
+            }
+
+            if (rowCnt >= m) {
+                ans++;
+            }
+
+            if (colCnt >= m) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
