05d5a6297095fe7ded865625645614fab2115391
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-07-29 18:17:45 +0900

    Title: 삼각형 컨베이어 벨트, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240729/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        Deque<Integer> que = new ArrayDeque<>();
+
+        for(int i = 0; i < n * 3; i++) {
+            que.add(sc.nextInt());
+        }
+
+        while(t-- > 0) {
+            int num = que.pollLast();
+            que.offerFirst(num);
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print(que.poll() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
