6c1c53b3f0ed7b334a10cfd37fef94e140c5dfec
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-07-29 17:59:24 +0900

    Title: 행복한 수열의 개수, Time: 292ms, Memory: 20MB, Status: Passed - CodeTree

--- 240729/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240729/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,59 +1,59 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n, m;
-    static int ans;
+    public static final int MAX_N = 100;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    
+    public static int[] seq = new int[MAX_N];
+    
+    public static boolean isHappySequence(){
+        // 주어진 seq가 행복한 수열인지 판단하는 함수입니다.
+        int consecutiveCount = 1, maxCcnt = 1;
+        for(int i = 1; i < n; i++) {
+            if (seq[i - 1] == seq[i])
+                consecutiveCount++;
+            else
+                consecutiveCount = 1;
+            
+            maxCcnt = Math.max(maxCcnt, consecutiveCount);
+        }
+        
+        // 최대로 연속한 회수가 m이상이면 true를 반환합니다. 
+        return maxCcnt >= m;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
         m = sc.nextInt();
-        int[][] matrix = new int[n][n];
 
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n; j++)
-                matrix[i][j] = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
-
-        for (int i = 0; i < n; i++) {
-            int rowNum = matrix[0][i];
-            int colNum = matrix[i][0];
-            int rowCnt = 0;
-            int colCnt = 0;
-            boolean rowCheck = false;
-            boolean colCheck = false;
-
-            for (int j = 0; j < n; j++) {
-
-                if (matrix[i][j] == colNum) {
-                    colCnt++;
-                } else {
-                    colCnt = 1;
-                    colNum = matrix[i][j];
-                }
-                
-                if (matrix[j][i] == rowNum) {
-                    rowCnt++;
-                } else {
-                    rowCnt = 1;
-                    rowNum = matrix[j][i];
-
-                }
-
-                if (rowCnt >= m) {
-                    rowCheck = true;
-                }
-
-                if (colCnt >= m) {
-                    colCheck = true;
-                }
-            }
-
-            if (rowCheck) ans++;
-            if (colCheck) ans++;
+        int numHappy = 0;
+        
+        // 먼저 가로로 행복한 수열의 수를 셉니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++)
+                seq[j] = grid[i][j];
+            
+            if(isHappySequence())
+                numHappy++;
+        }
+        
+        // 세로로 행복한 수열의 수를 셉니다.
+        for(int j = 0; j < n; j++){
+            // 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+            for(int i = 0; i < n; i++)
+                seq[i] = grid[i][j];
+            
+            if(isHappySequence())
+                numHappy++;
         }
 
-        System.out.println(ans);
+        System.out.print(numHappy);
     }
 }
