0f94a463e0aac955c02dce71b574992da0787244
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-07-30 22:00:38 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/기울어진 직사각형/slanted-rectangle.java
+++ 240730/기울어진 직사각형/slanted-rectangle.java
@@ -4,13 +4,18 @@ public class Main {
     static int n;
     static int ans = 0;
     static int[][] matrix;
+    static int startRow, startCol;
+    static int[] dx = {-1, -1, 1, 1};
+    static int[] dy = {1, -1, -1, 1};
 
     public static void main(String[] args) {
         init();
 
-        for(int i = 1; i < n - 1; i++) {
-            for(int j = 1; j < n - 1; j++) {
-                solve(i + 1, j);
+        for(int i = 2; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                startRow = i;
+                startCol = j;
+                solve(i, j, 0, 0);
             }
         }
 
@@ -30,35 +35,36 @@ public class Main {
         }
     }
 
-    public static void solve(int row, int col) {
-        int sumValue = 0;
-        int tempRow = row;
-        int tempCol = col;
-
-        int[] dx = {-1, -1, 1, 1};
-        int[] dy = {1, -1, -1, 1};
-
-        for(int dir = 0; dir < 4; dir++) {
-            while(true) {
-                int nx = tempRow + dx[dir];
-                int ny = tempCol + dy[dir];
-
-                if(!inRange(nx, ny)) break;
-                sumValue += matrix[tempRow][tempCol];
-                tempRow = nx;
-                tempCol = ny;
-
-                if(sumValue > matrix[row][col] && tempRow == row && tempCol == col) {
-                    ans = Math.max(ans, sumValue);
-                    break;
-                }
+    public static void solve(int row, int col, int dir, int value) {
+        if(matrix[startRow][startCol] < value) {
+            if(startRow == row && startCol == col) {
+                ans = Math.max(value, ans);
+                return;
             }
+            if(startRow <= row && startCol <= col) return;
+        }
+
+        value += matrix[row][col];
+
+        // 직진 가능 시 직진
+        int nx = row + dx[dir];
+        int ny = col + dy[dir];
+        if(inRange(nx, ny)) {
+            solve(nx, ny, dir, value);
+        }
+
+        // 방향전환 가능 시 방향전환
+        dir = (dir + 1) % 4;
+        nx = row + dx[dir];
+        ny = col + dy[dir];
+        if(inRange(nx, ny)) {
+            solve(nx, ny, dir, value);
         }
     }
 
     public static boolean inRange(int row, int col) {
         if(0 <= row && row < n && 0 <= col && col < n) {
-            if((row == 0 || row == n - 1 || row == -(n - 1)) && (col == 0 || col == n - 1 || col == -(n - 1))) return false;
+            if((row == 0 || row == n || row == -n) && (col == 0 || col == n || col == -n)) return false;
             return true;
         }
         return false;
