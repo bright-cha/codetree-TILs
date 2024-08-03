c62fae40b4d9c189f8ef09f98dbb7a6d218f9383
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-04 02:16:15 +0900

    Title: 핀볼게임, Time: 288ms, Memory: 19MB, Status: Passed - CodeTree

--- 240804/핀볼게임/pinball-game.java
+++ 240804/핀볼게임/pinball-game.java
@@ -1,94 +1,54 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int ans = 0;
-    static int[][] grid;
-    static int[] startX;
-    static int[] startY;
-    // 오, 왼, 위, 아래
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 100;
+    
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+    
+    public static int calc(int x, int y, int moveDir) {    
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+        // 1번 블럭에서는 방향이 다음과 같이 변합니다 : 0<->3 1<->2
+        // 2번 블럭에서는 방향이 다음과 같이 변합니다 : 0<->2 1<->3
+        
+        int elapsedTime = 1;
+        
+        while(inRange(x, y)) {
+            if(grid[x][y] == 1)
+                moveDir = 3 - moveDir;
+            else if(grid[x][y] == 2)
+                moveDir = (moveDir < 2) ? (moveDir + 2) : (moveDir - 2);
+            x += dx[moveDir]; y += dy[moveDir];
+            elapsedTime++;
+        }
+        return elapsedTime;
+    }
 
     public static void main(String[] args) {
-        init();
-
-        for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < n; j++) {
-                solve(i, j);
-            }
-        }
-
-        System.out.println(ans);
-    }
-
-    private static void solve(int dir, int idx) {
-        int time = 1;
-        int x = startX[dir];
-        int y = startY[dir];
-
-        if(dir == 0 || dir == 1) x *= idx;
-        if(dir == 2 || dir == 3) y *= idx;
-
-        while(inRange(x, y)) {
-            if(grid[x][y] == 1) dir = meetOne(dir);
-            if(grid[x][y] == 2) dir = meetTwo(dir);
-
-            x += dx[dir];
-            y += dy[dir];
-
-            time++;
-        }
-
-        ans = Math.max(time, ans);
-    }
-
-    private static int meetOne(int dir) {
-        switch(dir) {
-            case 0:
-                return 2;
-            case 1:
-                return 3;
-            case 2:
-                return 0;
-            case 3:
-                return 1;
-        }
-        return dir;
-    }
-
-    private static int meetTwo(int dir) {
-        switch(dir) {
-            case 0:
-                return 3;
-            case 1:
-                return 2;
-            case 2:
-                return 1;
-            case 3:
-                return 0;
-        }
-        return dir;
-    }
-
-    private static boolean inRange(int row, int col) {
-        if(0 <= row && row < n && 0 <= col && col < n) return true;
-        return false;
-    }
-
-    private static void init() {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
-        grid = new int[n][n];
-
-        startX = new int[] {1, 1, n - 1, 0};
-        startY = new int[] {0, n - 1, 1, 1};
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-            }
+        
+        // 각각의 상하좌우 방향에 대해
+        // 가능한 모든 위치에서 걸리는 시간을 계산한 후,
+        // 그 중 최댓값을 구합니다.
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans = Math.max(ans, calc(n - 1, i, 0));
+            ans = Math.max(ans, calc(0, i, 1));
+            ans = Math.max(ans, calc(i, n - 1, 2));
+            ans = Math.max(ans, calc(i, 0, 3));
         }
+        
+        System.out.print(ans);
     }
 }
