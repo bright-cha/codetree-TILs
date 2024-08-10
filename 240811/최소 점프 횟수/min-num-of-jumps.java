bedbbc5923f140ca9a4fb9a7cab1c5289a992381
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-11 03:47:26 +0900

    Title: 최소 점프 횟수, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240811/최소 점프 횟수/min-num-of-jumps.java
+++ 240811/최소 점프 횟수/min-num-of-jumps.java
@@ -1,36 +1,38 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int ans = Integer.MAX_VALUE;
-    static int[] arr;
-
-    public static void main(String[] args) {
-        init();
-        solve(0, 0);
-        if(ans == Integer.MAX_VALUE) ans = -1;
-        System.out.println(ans);
-    }
-
-    private static void solve(int cnt, int local) {
-        if(local >= n) return;
-        if(local == n - 1) {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 10;
+    
+    public static int n;
+    public static int[] num = new int[MAX_N];
+    
+    public static int ans = INT_MAX;
+    
+    public static void findMin(int idx, int cnt) {
+        // 마지막 위치를 넘었을 때
+        // 그 중 최소 이동 횟수를 갱신합니다.
+        if(idx >= n - 1) {
             ans = Math.min(ans, cnt);
             return;
         }
-
-        for(int i = 1; i <= arr[local]; i++) {
-            solve(cnt + 1, local + i);
-        }
+        
+        for(int dist = 1; dist <= num[idx]; dist++)
+            findMin(idx + dist, cnt + 1);
     }
 
-    private static void init() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+        
+        for(int i = 0; i < n; i++)
+            num[i] = sc.nextInt();
+        
+        findMin(0, 0);
+        
+        if(ans == INT_MAX)
+            ans = -1;
+        
+        System.out.print(ans);
     }
 }
