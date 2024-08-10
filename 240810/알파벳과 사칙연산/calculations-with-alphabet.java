f95ea6db916743e517918e76edfaa263bcea6006
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-10 20:04:07 +0900

    Title: 알파벳과 사칙연산, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240810/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 240810/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -1,41 +1,53 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static String str;
-    static int ans = Integer.MIN_VALUE;
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_N = 6;
+    
+    public static int n = 6;
+    
+    public static String expression;
+    public static int[] num = new int[MAX_N];
+    public static int ans = INT_MIN;
+    
+    public static int conv(int idx) {
+        return num[expression.charAt(idx) - 'a'];
+    }
+    
+    public static int calc() {
+        int length = (int) expression.length();
+        int value = conv(0);
+        for(int i = 2; i < length; i += 2) {
+            if(expression.charAt(i - 1) == '+')
+                value += conv(i);
+            else if(expression.charAt(i - 1) == '-')
+                value -= conv(i);
+            else
+                value *= conv(i);
+        }
+        return value;
+    }
+    
+    // 'a'부터 'f'까지 순서대로
+    // 0부터 5번째 index까지의 값을 
+    // 1~4 중에 하나로 채웁니다.
+    public static void findMax(int cnt) {
+        if(cnt == n) {
+            ans = Math.max(ans, calc());
+            return;
+        }
+        
+        for(int i = 1; i <= 4; i++) {
+            num[cnt] = i;
+            findMax(cnt + 1);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        str = sc.next();
-        int[] charToInt = new int[128];
-
-        solve((int) 'a', charToInt);
-        System.out.println(ans);
-    }
-
-    private static void solve(int local, int[] charToInt) {
-        if(local == ((int) 'f') + 1) {
-            int num = charToInt[str.charAt(0)];
-
-            for(int i = 2; i < str.length(); i += 2) {
-                char temp = str.charAt(i - 1);
-                if(temp == '-') {
-                    num -= charToInt[str.charAt(i)];
-                } else if(temp == '+') {
-                    num += charToInt[str.charAt(i)];
-                } else {
-                    num *= charToInt[str.charAt(i)];
-                }
-            }
-
-            ans = Math.max(ans, num);
-            return;
-        }
-
-        for(int i = 1; i <= 4; i++) {
-            charToInt[local] = i;
-            solve(local + 1, charToInt);
-            charToInt[local] = 1;
-        }
+        expression = sc.next();
+        
+        findMax(0);
+        System.out.print(ans);
     }
 }
