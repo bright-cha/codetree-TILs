9a11d09890173cd55c74e5319f33a55031733425
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-12 00:40:46 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 206ms, Memory: 12MB, Status: Passed - CodeTree

--- 240812/수들 중 최솟값 최대화하기/maximin-of-numbers.java
+++ 240812/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -1,45 +1,62 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    static int n;
-    static int ans = Integer.MIN_VALUE;
-    static int[][] grid;
-    static boolean[] visited;
-
-    public static void main(String[] args) {
-        init();
-        paint(0, Integer.MAX_VALUE);
-        System.out.println(ans);
-    }
-
-    public static void paint(int cnt, int value) {
-        if(cnt == n) {
-            ans = Math.max(value, ans);
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 10;
+    
+    // 변수 선언
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static boolean[] visited = new boolean[MAX_N];
+    public static ArrayList<Integer> picked = new ArrayList<>();
+    
+    public static int ans;
+    
+    // 현재 색칠된 칸을 선택할 행을 row라 했을 때 
+    // 계속 탐색을 이어서 진행합니다.
+    // 첫 번째 행에 색칠할 열의 위치부터
+    // 두 번째, 세 번째, .. n번째 행에 색칠할 열의 위치까지
+    // 각 열의 위치에 대한 순열을 만들어줍니다.
+    public static void findMax(int row) {
+        // 색칠된 칸에 적힌 수들 중 최솟값이
+        // 최대가 되는 경우를 계산합니다.
+        if(row == n) {
+            int minNum = INT_MAX;
+            for(int i = 0; i < picked.size(); i++)
+                minNum = Math.min(minNum, grid[i][picked.get(i)]);
+    
+            // 답을 갱신해줍니다.
+            ans = Math.max(ans, minNum);
             return;
         }
-
+    
+        // 현재 행에 색칠할 열을 선택합니다.
         for(int i = 0; i < n; i++) {
             if(visited[i]) continue;
-
             visited[i] = true;
-            paint(cnt + 1, Math.min(value, grid[cnt][i]));
+            picked.add(i);
+    
+            findMax(row + 1);
+    
             visited[i] = false;
+            picked.remove(picked.size() - 1);
         }
-
-        
     }
 
-    public static void init() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        // 입력:
         n = sc.nextInt();
-        grid = new int[n][n];
-        visited = new boolean[n];
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-            }
-        }
+
+        // 색칠된 칸에 적힌 수들 중 최솟값이 
+        // 최대가 되도록 탐색을 진행합니다.
+        findMax(0);
+
+        System.out.print(ans);
     }
 }
