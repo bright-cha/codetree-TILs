51977aee69533a6851fd77241291c523882a18c0
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-21 09:00:13 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240821/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int ans = 0;
+    static int[][] grid;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }        
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if (ans == 1) {
+                    System.out.println(ans);
+                    return;
+                }
+                bfs(i, j);
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void bfs(int x, int y) {
+        int[][] delta = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        boolean[][] visited = new boolean[n][m];
+
+        Queue<int[]> q = new ArrayDeque<>();
+        
+        q.offer(new int[] {x, y});
+        visited[x][y] = true;
+
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            x = node[0];
+            y = node[1];
+
+            if (x == n - 1 && y == n - 1) {
+                ans = 1;
+                return;
+            }
+
+            for(int i = 0; i < 4; i++) {
+                int nx = x + delta[i][0];
+                int ny = y + delta[i][1];
+
+                if (inRange(nx, ny) && !visited[nx][ny]) {
+                    if (grid[nx][ny] == 0) continue;
+                    visited[nx][ny] = true;
+                    q.offer(new int[] {nx, ny});
+                }
+            }
+        }
+    }
+
+    public static boolean inRange(int x, int y) {
+        if (0 <= x && x < n && 0 <= y && y < m) return true;
+        return false;
+    }
+}
