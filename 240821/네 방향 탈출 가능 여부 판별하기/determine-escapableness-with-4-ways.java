405d33815a9fb7c46df7d340c871d5ae172a93b1
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-21 09:34:53 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 336ms, Memory: 21MB, Status: Passed - CodeTree

--- 240821/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 240821/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,62 +1,77 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Pair { 
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+}
 
 public class Main {
-    static int n, m;
-    static int ans = 0;
-    static int[][] grid;
+    public static final int MAX_NUM = 100;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static int n, m;
+    public static int[][] a = new int[MAX_NUM][MAX_NUM];
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        grid = new int[n][m];
+    // bfs에 필요한 변수들 입니다.
+    public static boolean[][] visited = new boolean[MAX_NUM][MAX_NUM];
+    public static Queue<Pair> q = new LinkedList<>();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                grid[i][j] = sc.nextInt();
-            }
-        }        
-
-        bfs(0, 0);
-
-        System.out.println(ans);
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
     }
 
-    public static void bfs(int x, int y) {
-        int[][] delta = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        boolean[][] visited = new boolean[n][m];
+    public static boolean canGo(int x, int y) {
+        return inRange(x, y) && a[x][y] == 1 && !visited[x][y];
+    }
 
-        Queue<int[]> q = new ArrayDeque<>();
-        
-        q.offer(new int[] {x, y});
-        visited[x][y] = true;
-
+    public static void BFS() {
+         // queue에 남은 것이 없을때까지 반복합니다.
         while(!q.isEmpty()) {
-            int[] node = q.poll();
-            x = node[0];
-            y = node[1];
+            // queue에서 가장 먼저 들어온 원소를 뺍니다.
+            Pair curr = q.poll();
+            int x = curr.x, y = curr.y;
 
-            if (x == n - 1 && y == m - 1) {
-                ans = 1;
-                return;
-            }
-
+            int[] dx = new int[]{0, 1,  0, -1};
+            int[] dy = new int[]{1, 0, -1,  0};
+            
+            // queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
             for(int i = 0; i < 4; i++) {
-                int nx = x + delta[i][0];
-                int ny = y + delta[i][1];
+                int newX = x + dx[i];
+                int newY = y + dy[i];
 
-                if (inRange(nx, ny) && !visited[nx][ny]) {
-                    if (grid[nx][ny] == 0) continue;
-                    visited[nx][ny] = true;
-                    q.offer(new int[] {nx, ny});
+                // 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+                // 새로 queue에 넣어주고 방문 여부를 표시해줍니다. 
+                if(canGo(newX, newY)) {
+                    q.add(new Pair(newX, newY));
+                    visited[newX][newY] = true;
                 }
             }
         }
     }
 
-    public static boolean inRange(int x, int y) {
-        if (0 <= x && x < n && 0 <= y && y < m) return true;
-        return false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                a[i][j] = sc.nextInt();
+
+        // bfs를 이용해 최소 이동 횟수를 구합니다.
+        // 시작점을 queue에 넣고 시작합니다.
+        q.add(new Pair(0, 0));
+        visited[0][0] = true;
+
+        BFS();
+
+        // 우측 하단을 방문한 적이 있는지 여부를 출력합니다.
+        int answer = visited[n - 1][m - 1] ? 1 : 0;
+        System.out.println(answer);
     }
 }
