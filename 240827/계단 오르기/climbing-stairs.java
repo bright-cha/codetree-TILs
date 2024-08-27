760507861582767ede5b003af8ea2eacdd54f4f6
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-27 20:46:35 +0900

    Title: 계단 오르기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240827/계단 오르기/climbing-stairs.java
@@ -0,0 +1,29 @@
+import java.io.*;
+ import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 1000;
+    public static int[] dp = new int[MAX_N + 1];
+    public static int n;
+    
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        System.out.println(dp[n] % 10007);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+    }
+}
