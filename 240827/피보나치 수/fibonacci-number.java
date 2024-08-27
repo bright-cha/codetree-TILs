5d6dcda7f401258b386677201546103dc129d6ed
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-27 20:37:00 +0900

    Title: 피보나치 수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240827/피보나치 수/fibonacci-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static int[] arr = new int[46];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        System.out.println(arr[n]);
+    }
+
+}
