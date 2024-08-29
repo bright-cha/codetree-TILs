e0f6361a7b1f7b6768a3ac02aaa337b7b9fc8ce3
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-08-29 10:43:40 +0900

    Title: 2차원 최대 증가 수열, Time: 105ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240829/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 50;
+    public static final int MAX_M = 50;
+    public static final int MAX_VALUE = 10000;
+
+    public static int n, m, ans;
+    public static int[][] grid = new int[MAX_N][MAX_M];
+    public static int[][] dp = new int[MAX_N][MAX_M];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                for (int x = 0; x < i; x++) {
+                    for (int y = 0; y < j; y++) {
+                        if (grid[i][j] <= grid[x][y]) continue;
+
+                        dp[i][j] = Math.max(dp[i][j], dp[x][y] + 1);
+                    }
+                }
+
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        System.out.println(ans + 1);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
