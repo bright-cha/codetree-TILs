622d0c415af5ebb2ab5865943e5b41affd6d4351
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-09-30 14:59:48 +0900

    Title: 사각형 채우기 3, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240930/사각형 채우기 3/rectangle-fill-3.java
+++ 240930/사각형 채우기 3/rectangle-fill-3.java
@@ -1,30 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_N = 1000;
     public static final int MOD = 1000000007;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
     public static int n;
-    public static int[] dp = new int[MAX_N + 1];
+    
+    public static long[] dp = new long[MAX_N + 1];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-        n = Integer.parseInt(st.nextToken());
-
+        // 초기 조건 설정
         dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 7;
 
-        for (int i = 3; i <= n; i++) {
+        // 점화식에 따라 dp값 채우기
+        // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+        //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+        for(int i = 2; i <= n; i++) {
             dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
-            for (int j = i; j >= 3; j--) {
-                dp[i] += (dp[i - j] * 2) % MOD;
-            }
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
         }
-
-        System.out.println(dp[n]);
-
+        
+        System.out.print(dp[n]);
     }
 }
