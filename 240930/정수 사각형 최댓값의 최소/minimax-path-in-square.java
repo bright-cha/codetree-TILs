e126f5948f7a13c63382679944269563701a1a49
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-09-30 18:26:54 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240930/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_VALUE = 1000000;
+
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] dp = new int[MAX_N][MAX_N];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] = Math.max(dp[i][j], grid[i][j]);
+            }
+        }
+        
+        System.out.println(dp[n - 1][n - 1]);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i][0], dp[i - 1][0]);
+            dp[0][i] = Math.max(dp[0][i], dp[0][i - 1]);
+        }
+    }
+}
