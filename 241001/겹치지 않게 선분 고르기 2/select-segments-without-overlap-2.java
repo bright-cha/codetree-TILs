38fc6975978feddcf2d463e1a4ebb73976806646
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-01 15:12:02 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 119ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241001/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+class Segment implements Comparable<Segment> {
+    int x1;
+    int x2;
+
+    public Segment(int x1, int x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+
+    @Override
+    public int compareTo(Segment s) {
+        return this.x1 - s.x1;
+    }
+}
+
+public class Main {
+    public static final int MAX_N = 1000;
+    public static int n;
+    public static Segment[] segments = new Segment[MAX_N];
+    public static int[] dp = new int[MAX_N];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (segments[i].x2 < segments[j].x1) {
+                    dp[i] += 1;
+                }
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            segments[i] = new Segment(s, e);
+        }
+
+        Arrays.sort(segments, 0, n);
+    }
+}
