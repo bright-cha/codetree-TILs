6c8fc3bd6b7669e215653b0d8929911e7ac12628
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-02 02:04:02 +0900

    Title: 동전 거슬러주기, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241002/동전 거슬러주기/coin-change.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_M = 10000;
+    public static final int MAX_COST = 10000;
+
+    public static int n, m;
+    public static int[] coin = new int[MAX_N + 1];
+    public static int[] dp = new int[MAX_M + 1];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= coin[j]) {
+                    dp[i] = Math.min(dp[i], dp[i - coin[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[m] == Integer.MAX_VALUE ? -1 : dp[m]);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());        
+
+        for (int i = 1; i <= n; i++) {
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= m; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+    }
+}
