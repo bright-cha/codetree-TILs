4a124a58fce51cdf4f036ec523b06e481ab76c18
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-02 14:43:38 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 926ms, Memory: 39MB, Status: Passed - CodeTree

--- 241002/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 241002/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -1,40 +1,44 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_N = 100000;
-    public static final int MAX_VALUE = 1000;
     public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_N = 100000;
 
     public static int n;
-    public static int[] numArr = new int[MAX_N + 1];
-    public static int[] dp = new int[MAX_N + 1];
 
-    public static void main(String[] args) throws IOException {
-        init();
+    public static int[] a = new int[MAX_N + 1];
 
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Math.max(numArr[i], dp[i - 1] + numArr[i]);
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            a[i] = sc.nextInt();
+
+        // 최댓값을 구해야 하는 문제이므로
+        // 초기값을 INT_MIN으로 설정합니다.
         int ans = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            ans = Math.max(ans, dp[i]);
+
+        // 현재 연속 부분 수열 내 원소의 합을
+        // 저장합니다.
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            // 만약 현재 연속 부분 수열 내 원소의 합이
+            // 0보다 작아진다면, 지금부터 새로운
+            // 연속 부분 수열을 만드는 것이 더 유리합니다.
+            if(sum < 0)
+                sum = a[i];
+            
+            // 그렇지 않다면 기존 연속 부분 수열에 
+            // 현재 원소를 추가하는 것이 더 좋습니다.
+            else
+                sum += a[i];
+            
+            // 가능한 연속 부분 수열 중 최댓값을 찾아 갱신합니다.
+            ans = Math.max(ans, sum);
         }
 
         System.out.println(ans);
-    }
-
-    public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= n; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = INT_MIN;
-        }
     }
 }
