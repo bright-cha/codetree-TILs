0a68ba7f473d4a1331b0969b2ec2bbff2c2e29d4
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-02 19:54:30 +0900

    Title: 축구와 야구, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241002/축구와 야구/soccer-and-baseball.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+class Student {
+    // 축구 능력
+    int sa;
+    // 야구 능력
+    int ba;
+
+    public Student(int sa, int ba) {
+        this.sa = sa;
+        this.ba = ba;
+    }
+}
+
+public class Main {
+    public static final int MAX_N = 1000;
+    public static final int SOC_TOTAL = 11;
+    public static final int BAS_TOTAL = 9;
+
+    public static int n;
+    public static Student[] students = new Student[MAX_N + 1];
+    public static int[][][] dp = new int[MAX_N + 1][SOC_TOTAL + 1][BAS_TOTAL + 1];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= SOC_TOTAL; j++) {
+                for (int k = 1; k <= BAS_TOTAL; k++) {
+
+                    // 현재 학생이 축구에 선발
+                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - 1][k] + students[i].sa);
+
+                    // 현재 학생이 야구에 선발
+                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][k - 1] + students[i].ba);
+
+                    // 현재 학생이 선발되지 않음
+                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][k]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][SOC_TOTAL][BAS_TOTAL]);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int sa = Integer.parseInt(st.nextToken());
+            int ba = Integer.parseInt(st.nextToken());
+
+            students[i] = new Student(sa, ba);
+        }
+
+        // 1번 학생이 축구에 선발됨
+        dp[1][1][0] = students[1].sa;
+
+        // 1번 학생이 야구에 선발됨
+        dp[1][0][1] = students[1].ba;
+    }
+}
