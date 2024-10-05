a0dd39b7c1acdb1561160baa6a2b9ed0ae586367
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-05 14:33:50 +0900

    Title: 최단 공통 상위수열, Time: 130ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 241005/최단 공통 상위수열/shortest-common-supersubsequence.java
+++ 241005/최단 공통 상위수열/shortest-common-supersubsequence.java
@@ -7,37 +7,30 @@ public class Main {
     public static String a = null;
     public static String b = null;
 
-    public static int[] dp = new int[MAX_LEN + 1];
-    public static boolean[] visited = new boolean[MAX_LEN + 1];
+    public static int[][] dp = new int[MAX_LEN + 1][MAX_LEN + 1];
 
     public static void main(String[] args) throws IOException {
         init();
 
         // i번 문자와 j번 문자가 같으면 그대로 다르면 +1;
-        for (int i = 1; i <= lenA; i++) {
-            boolean find = false;
-
-            for (int j = 1; j <= lenB; j++) {
-
-                if (a.charAt(i) == b.charAt(j) && !visited[j]) {
-                    find = true;
-                    visited[j] = true;
-                    break;
+        for (int i = 2; i <= lenA; i++) {
+            for (int j = 2; j <= lenB; j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-
-            if (find) {
-                dp[i] = dp[i - 1];
-            } else {
-                dp[i] = dp[i - 1] + 1;
-            }
         }
 
-        for (int i = 1; i <= lenA; i++) {
-            System.out.print(dp[i] + " ");
-        }
+        // for (int j = 1; j <= lenB; j++) {
+        //     for (int i = 1; i <= lenA; i++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
         
-        System.out.println(dp[lenA] + lenB);
+        System.out.println(dp[lenA][lenB] + lenB);
     }
 
     public static void init() throws IOException {
@@ -49,5 +42,21 @@ public class Main {
         lenB = b.length();
         a = " " + a;
         b = " " + b;
+
+        for (int i = 1; i <= lenA; i++) {
+            if (a.charAt(i) == b.charAt(1)) {
+                dp[i][1] = dp[i - 1][1];
+            } else {
+                dp[i][1] = dp[i - 1][1] + 1;
+            }
+        }
+
+        for (int i = 1; i <= lenB; i++) {
+            if (a.charAt(1) == b.charAt(i)) {
+                dp[1][i] = dp[1][i - 1];
+            } else {
+                dp[1][i] = dp[1][i - 1] + 1;
+            }
+        }
     }
 }
