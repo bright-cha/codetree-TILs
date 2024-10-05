789f82f4768e6d2be6d2b586aed231b058edb336
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-05 16:35:21 +0900

    Title: 낮은 지점들, Time: 396ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 241005/낮은 지점들/lowest-points.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    public static final int MAX_VALUE = 1000000000;
+    public static int n;
+    public static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        Collection<Integer> values = map.values();
+
+        int ans = 0;
+        for (int value : values) {
+            ans += value;
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            if (map.containsKey(k)) {
+                int prev = map.get(k);
+                map.put(k, Math.min(prev, v));
+            } else {
+                map.put(k, v);
+            }
+        }
+    }
+}
