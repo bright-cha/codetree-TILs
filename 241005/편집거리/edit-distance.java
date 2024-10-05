0e364f4bc7f8a2f1b2e5689d7ba32c271fc74ad7
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-05 15:20:47 +0900

    Title: 편집거리, Time: 78ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 241005/편집거리/edit-distance.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LEN = 1000;
+
+    public static int lenA, lenB;
+    public static String a = " ";
+    public static String b = " ";
+    public static int[][] dp = new int[MAX_LEN + 1][MAX_LEN + 1];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 2; i <= lenA; i++) {
+            for (int j = 2; j <= lenB; j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        
+        // for (int j = 1; j <= lenB; j++) {
+        //     for (int i = 1; i <= lenA; i++) {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        System.out.println(dp[lenA][lenB]);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        a += br.readLine();
+        b += br.readLine();
+        lenA = a.length() - 1;
+        lenB = b.length() - 1;
+
+        for (int i = 1; i <= lenA; i++) {
+            
+            if (a.charAt(i) != b.charAt(1)) {
+                dp[i][1] = dp[i - 1][1] + 1;
+            } else {
+                dp[i][1] = dp[i - 1][1];
+            }
+
+            if (a.charAt(1) != b.charAt(i)) {
+                dp[1][i] = dp[1][i - 1] + 1;
+            } else {
+                dp[1][i] = dp[1][i - 1];
+            }
+        }
+
+
+    }
+}
