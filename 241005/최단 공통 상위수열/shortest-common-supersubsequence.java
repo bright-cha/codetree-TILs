9505f3c5fd61eb0b3254911f0f6e9732b2d09e0b
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-05 13:50:42 +0900

    Title: 최단 공통 상위수열, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241005/최단 공통 상위수열/shortest-common-supersubsequence.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static final int MAX_LEN = 1000;
+    public static int lenA, lenB;
+    public static String a = null;
+    public static String b = null;
+
+    public static int[] dp = new int[MAX_LEN + 1];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        // i번 문자와 j번 문자가 같으면 그대로 다르면 +1;
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    dp[i] = dp[i - 1];
+                } else {
+                    dp[i] = dp[i - 1] + 1;
+                }
+            }
+        }
+
+        System.out.println(dp[lenA] + lenB);
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        a = br.readLine();
+        b = br.readLine();
+        lenA = a.length();
+        lenB = b.length();
+        a = " " + a;
+        b = " " + b;
+    }
+}
