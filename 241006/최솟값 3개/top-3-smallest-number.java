5ed5ee65f5788b3b3fe298bbdc3e1fe7d4afb1d7
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-06 15:13:17 +0900

    Title: 최솟값 3개, Time: 104ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241006/최솟값 3개/top-3-smallest-number.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    public static final int MAX_VALUE = 100000;
+
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        for (int i = 0; i < n; i++) {
+            pq.add(arr[i]);
+
+            if (pq.size() < 3) {
+                System.out.println(-1);
+                continue;
+            }
+
+            int num1 = pq.poll();
+            int num2 = pq.poll();
+            int num3 = pq.poll();
+
+            pq.add(num1);
+            pq.add(num2);
+            pq.add(num3);
+
+            System.out.println(num1 * num2 * num3);
+        }
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
