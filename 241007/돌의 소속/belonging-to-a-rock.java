309944e6f4eb7cec2736787d3bb610ac8df820e1
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-07 14:29:02 +0900

    Title: 돌의 소속, Time: 2394ms, Memory: 44MB, Status: Passed - CodeTree

--- 241007/돌의 소속/belonging-to-a-rock.java
+++ 241007/돌의 소속/belonging-to-a-rock.java
@@ -5,11 +5,13 @@ public class Main {
     public static final int MAX_N = 100000;
     public static final int MAX_Q = 100000;
     public static int n, q;
-    public static HashMap<Integer, Integer> hm = new HashMap<>();
+    public static int[] nums = new int[MAX_N + 1];
+    public static int[] firsts = new int[MAX_N + 1];
+    public static int[] seconds = new int[MAX_N + 1];
+    public static int[] thirties = new int[MAX_N + 1];
 
     public static void main(String[] args) throws IOException {
         init();
-        
     }
 
     public static void init() throws IOException {
@@ -20,8 +22,25 @@ public class Main {
         q = Integer.parseInt(st.nextToken());
 
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            hm.put(i, num);
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            firsts[i] = firsts[i - 1];
+            seconds[i] = seconds[i - 1];
+            thirties[i] = thirties[i - 1];
+
+            switch (nums[i]) {
+                case 1:
+                    firsts[i]++;
+                    break;
+                case 2:
+                    seconds[i]++;
+                    break;
+                case 3:
+                    thirties[i]++;
+                    break;
+            }
         }
 
         for (int i = 0; i < q; i++) {
@@ -30,24 +49,7 @@ public class Main {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            int first = 0, second = 0, third = 0;
-            for (int j = start; j <= end; j++) {
-                int line = hm.get(j);
-                
-                switch (line) {
-                    case 1:
-                    first++;
-                    break;
-                    case 2:
-                    second++;
-                    break;
-                    case 3:
-                    third++;
-                    break;
-                }
-            }
-
-            System.out.println(first + " " + second + " " + third);
+            System.out.println((firsts[end] - firsts[start - 1]) + " " + (seconds[end] - seconds[start - 1]) + " " + (thirties[end] - thirties[start - 1]));
         }
     }
 }
