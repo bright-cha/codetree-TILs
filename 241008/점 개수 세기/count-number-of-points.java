b6a9973740820863e0aebc751f32bd8629735a0d
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-08 15:37:56 +0900

    Title: 점 개수 세기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241008/점 개수 세기/count-number-of-points.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    public static final int MAX_Q = 100000;
+
+    public static int n, q;
+    public static TreeSet<Integer> numSet = new TreeSet<>();
+    public static Map<Integer, Integer> sequenceMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            numSet.add(num);
+        }
+
+        int cnt = 1;
+        for (int num : numSet) {
+            sequenceMap.put(num, cnt);
+            cnt++;
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            if (numSet.first() <= start) {
+                start = numSet.ceiling(start) - 1;
+            } else {
+                start = 0;
+            }
+
+            if (numSet.last() >= end) {
+                end = numSet.floor(end);
+            } else {
+                end = numSet.size();
+            }
+
+            System.out.println(end - start);
+        }
+    }
+}
