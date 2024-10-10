e28b314568513c82c91ed0bcd793c75fb7d6f4d7
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-10 13:03:19 +0900

    Title: 마라톤 중간에 택시타기, Time: 1155ms, Memory: 54MB, Status: Passed - CodeTree

--- 241010/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
+++ 241010/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
@@ -1,78 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-class Pair {
-    int x;
-    int y;
-
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-public class Main {
+public class Main {    
+    public static final int INT_MAX = Integer.MAX_VALUE;
     public static final int MAX_N = 100000;
+    
+    // 변수 선언
     public static int n;
-    public static Pair[] points = new Pair[MAX_N + 1];
-    public static int[] leftSum = new int[MAX_N + 1];
-    public static int[] rightSum = new int[MAX_N + 1];
+    public static int[] x = new int[MAX_N];
+    public static int[] y = new int[MAX_N];
+    public static int[] L = new int[MAX_N];
+    public static int[] R = new int[MAX_N];
+    
+    public static int ans = INT_MAX;
 
-    public static void main(String[] args) throws IOException {
-        init();
-        findAns();
-    }
-
-    public static void findAns() {
-        int ans = Integer.MAX_VALUE;
-        for (int i = 2; i < n; i++) {
-            ans = Math.min(ans, leftSum[i - 1] + rightSum[i + 1] + saveTaxiDistance(points[i - 1], points[i + 1]));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
         }
+        
+        // L 배열을 채워줍니다.
+        // L[i] = 0번부터 i번까지 빠지는 부분 없이
+        //        순서대로 방문하기 위해
+        //        이동해야 하는 거리의 합
+        L[0] = 0;
+        for(int i = 1; i < n; i++)
+            L[i] = L[i - 1] + Math.abs(x[i] - x[i - 1]) + Math.abs(y[i] - y[i - 1]);
+        
+        // R 배열을 채워줍니다.
+        // R[i] = i번부터 n - 1번까지 빠지는 부분 없이
+        //        순서대로 방문하기 위해
+        //        이동해야 하는 거리의 합
+        R[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--)
+            R[i] = R[i + 1] + Math.abs(x[i + 1] - x[i]) + Math.abs(y[i + 1] - y[i]);
+        
+        // 각 체크포인트 마다
+        // 건너 뛰었다고 했을 때,
+        // 가능한 거리 합 중 최솟값을 계산합니다.
+        for(int i = 1; i < n - 1; i++)
+            ans = Math.min(ans, L[i - 1] + R[i + 1] + Math.abs(x[i + 1] - x[i - 1]) + Math.abs(y[i + 1] - y[i - 1]));
 
-        System.out.println(ans);
-
-
-        // for (int i = 0; i <= n; i++) {
-        //     System.out.print(leftSum[i] + " ");
-        // }
-        // System.out.println();
-
-        // for (int i = 0; i <= n; i++) {
-        //     System.out.print(rightSum[i] + " ");
-        // }
-    }
-
-    // 두 점의 택시거리 구하기
-    public static int saveTaxiDistance(Pair a, Pair b) {
-        // System.out.println(a.x + " " + a.y + " 부터 " + b.x + " " + b.y + " 까지는 " + (Math.abs(a.x - b.x) + Math.abs(a.y + b.y)));
-        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-    }
-
-    public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            points[i] = new Pair(x, y);
-        }
-
-        // i까지 포함된 왼쪽
-        for (int i = 2; i <= n; i++) {
-            Pair pair1 = points[i];
-            Pair pair2 = points[i - 1];
-            leftSum[i] = leftSum[i - 1] + saveTaxiDistance(pair1, pair2);
-        }
-
-        // i까지 포함된 오른쪽
-        for (int i = n - 1; i >= 1; i--) {
-            Pair pair1 = points[i];
-            Pair pair2 = points[i + 1];
-            rightSum[i] = rightSum[i + 1] + saveTaxiDistance(pair1, pair2);
-        }
+        System.out.print(ans);
     }
 }
