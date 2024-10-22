4fd1961fd30478bee6c4ec8a8b40c00d32a9db27
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-22 13:37:53 +0900

    Title: 삼 오 무, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 241022/삼 오 무/three-five-moo.java
+++ 241022/삼 오 무/three-five-moo.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static final int MAX_N = 1000000000;
+    public static final int INT_MAX = 2000000000;
     public static int n;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        int left = 1; 
-        int right = MAX_N;
-        int ans = MAX_N;
+        long left = 1; 
+        long right = INT_MAX;
+        long ans = INT_MAX;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            int num = (mid / 3) + (mid / 5) - (mid / 15);
+            long mid = (left + right) / 2;
+            long num = (mid / 3) + (mid / 5) - (mid / 15);
 
             if (mid - num < n) {
                 left = mid + 1;
