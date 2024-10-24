e62b0bd07db9940c0a065485099268f49a20b72a
Bright-cha <111628069+bright-cha@users.noreply.github.com>
2024-10-24 17:29:19 +0900

    Title: 수영장 효율적으로 활용하기, Time: 108ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241024/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static final int MAX_N = 100000;
+    public static final int MAX_M = 100000;
+    public static final int MAX_TIME = 1440;
+    public static int n, m;
+    public static int[] times = new int[MAX_N];
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        int left = 0;
+        int right = n * MAX_TIME;
+        int ans = Integer.MAX_VALUE;
+
+        while (left <= right) {
+            // 한 레인 당 최대 이용시간
+            int mid = (left + right) / 2;
+
+            int time = 0;
+            int line = 1;
+            for (int i = 0; i < n; i++) {
+                if (time + times[i] > mid) {
+                    line++;
+                    time = 0;
+                }
+                time += times[i];
+            }
+
+            // System.out.println(left + " " + right + " " + mid + " " + line);
+
+            if (line <= m) {
+                right = mid - 1;
+                ans = Math.min(ans, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
