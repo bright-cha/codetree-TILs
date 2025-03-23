93394641811e224458a91a703aa9e7819282aee9
Bright-cha <ckguscjf03@gmail.com>
2025-03-23 20:54:10 +0900

    Title: 최대 수 만들기, Time: 288ms, Memory: 22MB, Status: Runtime Error - CodeTree

---
+++ 250323/최대 수 만들기/make-biggest-num.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                String strA = String.valueOf(a);
+                String strB = String.valueOf(b);
+
+                if (Integer.valueOf(strA + strB) > Integer.valueOf(strB + strA)) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
